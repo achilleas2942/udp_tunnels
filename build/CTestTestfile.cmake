44296467f4395d58cfc9efbbdf3075b4d6392d9e
Achilleas Seisa <achilleas2942@gmail.com>
2024-06-17 12:14:24 +0200

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/oem/catkin_ws/src/udp_tunnels
+# Build directory: /home/oem/catkin_ws/src/udp_tunnels/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ src/udp_client_sbl.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# This script runs at the cloud/edge: sends the command messages to the agents (done)
+
+import rospy
+import socket
+import pickle
+from sensor_msgs.msg import Range
+
+
+class SendData(socket.socket):
+
+    def __init__(self, uri, port_num) -> None:
+        self.uri = uri
+        self.port_num = port_num
+        self.addr_info = socket.getaddrinfo(uri, port_num, proto=socket.IPPROTO_UDP)
+        super().__init__(
+            self.addr_info[0][0], self.addr_info[0][1], self.addr_info[0][2]
+        )
+        rospy.loginfo(
+            "Socket created uri: {sock_uri}, port num: {port_num}".format(
+                sock_uri=uri, port_num=port_num
+            )
+        )
+
+    def send_data(self, message):
+        self.sendto(message, (self.uri, self.port_num))
+
+
+def stack_data(data, socket):
+    message = pickle.dumps(data)
+    socket.send_data(message)
+
+
+if __name__ == "__main__":
+    try:
+        capturer_node = rospy.init_node("udp_client_sbl", anonymous=True)
+        # public ip of edge vm
+        socket = SendData("130.240.22.40", 30102)
+        # sbl (altimeter) topic
+        topic = "/sbl"
+        subscriber = rospy.Subscriber(topic, Range, stack_data, callback_args=socket)
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
